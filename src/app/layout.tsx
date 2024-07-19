c22d50b839728dc48afcb4941aa9728821d969b3
Alexander <s555b@mail.ru>
2024-07-19 14:54:31 +0300

    Add verification tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,11 @@ export const metadata: Metadata = {
   description: (
     "Клуб Японской Культуры РТУ МИРЭА ждет новых участников!\n" +
     "Фестивали, турниры, показы фильмов - всё это и еще много мероприятий, которые проводит наш клуб."
-  )
+  ),
+  verification: {
+    google: 'fLfnDHcR31b2YYlMb3WYM15oADMd6BDV8IJ_B8-BgZo',
+    yandex: '9f205069c0823269'
+  }
 };
 
 export default function Layout({children}: Readonly<{ children: React.ReactNode; }>) {
